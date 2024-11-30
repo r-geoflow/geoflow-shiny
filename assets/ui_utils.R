4df4bfa284520919ab4ddfb194a45665a5032f09
eblondel <emmanuel.blondel1@gmail.com>
2024-12-01 00:40:59 +0100

    add R version + RAM/CPU in app footer

--- assets/ui_utils.R
+++ assets/ui_utils.R
@@ -1,6 +1,6 @@
 footer <- function(id, version, date){
   tags$div(
     tags$p(sprintf("%s - v%s (%s)", id, version, date), style = "float:left;color:white;"),
-    tags$p("", style = "float:right;")
+    tags$p(paste(sessionInfo()$R.version$version.string,"– Resources: RAM:", round(benchmarkme::get_ram()/1e9,1),"GB", "–","CPU:", benchmarkme::get_cpu()$no_of_cores, "cores"), style = "float:right;")
   )
 }
